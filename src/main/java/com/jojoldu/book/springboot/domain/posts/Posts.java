2408ffe493d41efa5633cd5cc96cdcb86a90652c
saebak <okij3307@gmail.com>
2021-08-28 23:45:31 +0900

    add jpa code (regist, update)

--- src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.posts;
 
+import com.jojoldu.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,7 +14,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts {
+public class Posts extends BaseTimeEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,5 +34,11 @@ public class Posts {
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+
 
 }

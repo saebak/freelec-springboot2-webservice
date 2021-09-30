2b14d43088da84c61756acc59b7fa122cb88c20e
saebak <okij3307@gmail.com>
2021-10-01 00:02:21 +0900

    add naver login

--- src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -24,6 +24,10 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
+       if("naver".equals(registrationId)){
+           return ofNaver("id", attributes);
+       }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -35,6 +39,19 @@ public class OAuthAttributes {
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("picture"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+
     }
 
     public User toEntity() {
